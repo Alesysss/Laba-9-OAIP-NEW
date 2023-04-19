6adb968c798b9a3e72d1f771e831337286f0b55b
Alessys <Alessys@ALESYA>
2023-04-19 17:47:05 +0300

    добавила 4 класса с операторвми и операндами

--- Laba 9 OAIP NEW/Form1.Designer.cs
+++ Laba 9 OAIP NEW/Form1.Designer.cs
@@ -28,19 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
+            pictureBox = new PictureBox();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox).BeginInit();
             SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            pictureBox.BackColor = Color.White;
+            pictureBox.Location = new Point(153, 99);
+            pictureBox.Name = "pictureBox";
+            pictureBox.Size = new Size(969, 523);
+            pictureBox.TabIndex = 0;
+            pictureBox.TabStop = false;
+            pictureBox.Click += pictureBox_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Impact", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(32, 13);
+            label1.Name = "label1";
+            label1.Size = new Size(258, 37);
+            label1.TabIndex = 1;
+            label1.Text = "Командная строка";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(21, 53);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(292, 27);
+            textBox1.TabIndex = 2;
+            textBox1.TextChanged += textBox1_TextChanged;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = Color.PaleTurquoise;
+            ClientSize = new Size(1470, 667);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(pictureBox);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox pictureBox;
+        private Label label1;
+        private TextBox textBox1;
     }
 }
--- Laba 9 OAIP NEW/Form1.cs
+++ Laba 9 OAIP NEW/Form1.cs
@@ -1,13 +1,42 @@
+using System.Windows.Forms;
+
 namespace Laba_9_OAIP_NEW
 {
     public partial class Form1 : Form
     {
+        PointF[] pointFs = new PointF[3];
         public Form1()
         {
             InitializeComponent();
+            InitializeComponent();
+            Bitmap bitmap = new Bitmap(pictureBox.ClientSize.Width, pictureBox.ClientSize.Height);
+            Pen pen = new Pen(Color.Black, 1);
+            Init.bitmap = bitmap;
+            Init.pen = pen;
+            Init.pictureBox = pictureBox;
+        }
+        private Stack<Operator> operators = new Stack<Operator>();
+        private Stack<Operand> operands = new Stack<Operand>();
+        private Triangle figure;
+        private bool flag;
+        string Name;
+        string Name1;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void pictureBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyDown(object sender, EventArgs e)
         {
 
         }
